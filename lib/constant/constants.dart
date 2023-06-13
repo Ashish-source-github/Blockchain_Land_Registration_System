8af967633d2686279fbd55ccc15057ae798fdd0f
ASHISH AGARWAL <68642819+Ashish-source-github@users.noreply.github.com>
2023-06-14 00:01:46 +0530

    Add files via upload

---
+++ lib/constant/constants.dart
@@ -0,0 +1,13 @@
+String mapBoxApiKey =
+    'pk.eyJ1IjoiZ2FrYW5zaCIsImEiOiJjbGFpOTMzdngwMGVpM29zOTlpb3p0ZHhxIn0.alGGCYAXxU8q41E-VbjbHw';
+
+String nftStorageApiKey =
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDA5OTg2RUI5QzdBQkREMzQ0YTQ1YjUyMjIxQzhlNDZlZDlhOTk5QmEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2ODUxODc5MTI5MiwibmFtZSI6IkhBSEEifQ._LHdLDiKQKvbXSW5bkRt7pNYlPiZIH18XtYiWohQ5ZA';
+
+const String rpcUrl =
+    //"https://ropsten.infura.io/v3/e43345b7383246378963be7acd5b6c67";
+"http://127.0.0.1:7545";
+
+const String contractAddress = "0xcD258fBe029d2f116E689C6B00fa1445322B2C10";
+
+const int chainId = 1337; //Ropsten-3, Polygon testnet-80001, local ganache-1337
